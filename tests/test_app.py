9ba96f13b69c0020cde5498bcc75d3b6bab0d42c
Wil31 <w.gavigniaux@gmail.com>
2022-07-22 12:19:33 +0200

    flake8 and black passed

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,9 +18,7 @@ def test_should_return_index(client, captured_templates):
 
 
 def test_login_email(client, captured_templates):
-    rv = client.post(
-        "showSummary", data=dict(email="john@simplylift.co")
-    )
+    rv = client.post("showSummary", data=dict(email="john@simplylift.co"))
     assert rv.status_code == 200
 
     assert len(captured_templates) == 1
@@ -31,9 +29,7 @@ def test_login_email(client, captured_templates):
 
 
 def test_login_unknown_email(client, captured_templates):
-    rv = client.post(
-        "showSummary", data=dict(email="unknown_email@nope.com")
-    )
+    rv = client.post("showSummary", data=dict(email="unknown_email@nope.com"))
     assert rv.status_code == 403
 
     assert len(captured_templates) == 1
@@ -44,9 +40,7 @@ def test_login_unknown_email(client, captured_templates):
 
 
 def test_login_no_email(client, captured_templates):
-    rv = client.post(
-        "showSummary", data=dict(email="")
-    )
+    rv = client.post("showSummary", data=dict(email=""))
     assert rv.status_code == 403
 
     assert len(captured_templates) == 1
@@ -74,7 +68,9 @@ def test_book_nonexistant_competition_should_404(client):
     assert response.status_code == 404
 
 
-def test_book_nonexistant_competition_should_return_404_template(client, captured_templates):
+def test_book_nonexistant_competition_should_return_404_template(
+    client, captured_templates
+):
     response = client.get("/book/nonexistant comp/Simply Lift")
 
     assert len(captured_templates) == 1
@@ -101,16 +97,15 @@ def test_purchase_places(client, captured_templates):
     club_points = 13
     placesRequired = 2
     response = client.post(
-        "purchasePlaces", data=dict(competition=competition,
-                                    club=club,
-                                    places=str(placesRequired))
+        "purchasePlaces",
+        data=dict(competition=competition, club=club, places=str(placesRequired)),
     )
     assert response.status_code == 200
 
     assert len(captured_templates) == 1
     template, context = captured_templates[0]
     assert template.name == "welcome.html"
-    assert context['club']['points'] == club_points - placesRequired
+    assert context["club"]["points"] == club_points - placesRequired
 
 
 def test_purchase_more_places_than_club_points_should_406(client, captured_templates):
@@ -119,16 +114,15 @@ def test_purchase_more_places_than_club_points_should_406(client, captured_templ
     club_points = 13
     placesRequired = 14
     response = client.post(
-        "purchasePlaces", data=dict(competition=competition,
-                                    club=club,
-                                    places=str(placesRequired))
+        "purchasePlaces",
+        data=dict(competition=competition, club=club, places=str(placesRequired)),
     )
     assert response.status_code == 406
 
     assert len(captured_templates) == 1
     template, context = captured_templates[0]
     assert template.name == "welcome.html"
-    assert int(context['club']['points']) == club_points
+    assert int(context["club"]["points"]) == club_points
 
 
 def test_purchase_more_than_12_places_at_once_should_406(client, captured_templates):
@@ -137,42 +131,40 @@ def test_purchase_more_than_12_places_at_once_should_406(client, captured_templa
     club_points = 13
     placesRequired = 13
     response = client.post(
-        "purchasePlaces", data=dict(competition=competition,
-                                    club=club,
-                                    places=str(placesRequired))
+        "purchasePlaces",
+        data=dict(competition=competition, club=club, places=str(placesRequired)),
     )
     assert response.status_code == 406
 
     assert len(captured_templates) == 1
     template, context = captured_templates[0]
     assert template.name == "welcome.html"
-    assert int(context['club']['points']) == club_points
+    assert int(context["club"]["points"]) == club_points
 
 
-def test_purchase_more_than_12_places_in_competition_should_406(client, captured_templates):
+def test_purchase_more_than_12_places_in_competition_should_406(
+    client, captured_templates
+):
     competition = "Spring Festival"
     club = "Simply Lift"
-    club_points = 13
     placesRequired1 = 7
     placesRequired2 = 6
     response = client.post(
-        "purchasePlaces", data=dict(competition=competition,
-                                    club=club,
-                                    places=str(placesRequired1))
+        "purchasePlaces",
+        data=dict(competition=competition, club=club, places=str(placesRequired1)),
     )
     assert response.status_code == 200
 
     response = client.post(
-        "purchasePlaces", data=dict(competition=competition,
-                                    club=club,
-                                    places=str(placesRequired2))
+        "purchasePlaces",
+        data=dict(competition=competition, club=club, places=str(placesRequired2)),
     )
     assert response.status_code == 406
 
     assert len(captured_templates) == 2
     template, context = captured_templates[1]
     assert template.name == "welcome.html"
-    assert int(context['club']['points']) == 6
+    assert int(context["club"]["points"]) == 6
 
 
 def test_booking_outdated_comp_should_return_406(client, captured_templates):
@@ -194,15 +186,15 @@ def test_should_return_clubs_board(client, captured_templates):
     assert len(captured_templates) == 1
     template, context = captured_templates[0]
     assert template.name == "clubs_board.html"
-    assert len(context['clubs']) == number_of_clubs
+    assert len(context["clubs"]) == number_of_clubs
 
 
 def test_logout_should_status_code_302(client):
-    response = client.get(url_for('logout'))
+    response = client.get(url_for("logout"))
     assert response.status_code == 302
 
 
 def test_logout_should_return_index(client):
-    response = client.get(url_for('logout'), follow_redirects=True)
+    response = client.get(url_for("logout"), follow_redirects=True)
 
-    assert response.request.path == url_for('index')
+    assert response.request.path == url_for("index")
