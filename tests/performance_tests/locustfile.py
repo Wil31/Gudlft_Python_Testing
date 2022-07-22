5dad1ccbd1e9d6cfffc25753887389907d5421fe
Wil31 <w.gavigniaux@gmail.com>
2022-07-22 14:26:06 +0200

    locustfile update

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -8,24 +8,11 @@ class ProjectPerfTest(HttpUser):
 
     @task
     def login(self):
-        self.client.post(
-            "showSummary", data=dict(email="john@simplylift.co")
-        )
+        self.client.post("showSummary", data=dict(email="john@simplylift.co"))
 
     @task
     def booking(self):
         self.client.get("book/Spring Festival/Simply Lift")
-
-    # @task
-    # def purchase_places(self):
-    #     competition = "Spring Festival"
-    #     club = "Simply Lift"
-    #     placesRequired = 2
-    #     self.client.post(
-    #         "purchasePlaces", data=dict(competition=competition,
-    #                                     club=club,
-    #                                     places=str(placesRequired))
-    #     )
 
     @task
     def clubsBoard(self):
